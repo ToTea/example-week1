50a870b470cb17686999b71b9e5ca13d412a2e39
ToTea <koniarone445440@gmail.com>
2014-05-02 22:30:09 +0800

    add app.js

--- app.js
+++ app.js
@@ -8,21 +8,35 @@
 var http = require("http");
 var port = 1337;
 var request = require("request");
+var fs = require("fs");
 var url = "http://graph.facebook.com/Boo/photos?type=uploaded";
 
 http.createServer(function (req, res) {
+  console.log("receive a request.");
   res.writeHeader(200, {"Content-Type": "text/html"});
   
-  var data = "<html><head></head><body>"
-  request.get(url, function (err, body, response) {
+  var data = "";
+  var pic = [];
 
-    response = JSON.parse(response);
-    response.data.forEach(function (val, idx) {
-      data += "<img src='" + val.images[2].source + "'>";
+  fs.readFile('./index.html', function(err, indData){
+    data = indData;
+    data += "var data = [";
+
+    request.get(url, function (err, body, response) {
+
+      response = JSON.parse(response);
+      response.data.forEach(function (val, idx) {
+        pic.push(val.images[2].source);
+      });
+      
+      for(i = 0; i < pic.length-1; i+=1){
+        data += "'" + pic[i] + "', ";
+      }
+      data += "'" + pic[pic.length-1] + "'];";
+      data += "$('<img class=\"myimg\">').appendTo($('.pic')).attr('src', data[0]);"
+      data += "</script></body></html>";
+      res.end(data);
     });
-    
-    data += "</body></html>";
-    res.end(data);
   });
 
 }).listen(port);
